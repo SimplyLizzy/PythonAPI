0b6e9de11cfaeb13938160ccb68dda5cf37f73dd
SimplyLizzy <elingu94@gmail.com>
2022-02-09 21:55:11 -0600

    i messed up, again

--- Instructions/starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-# https://home.openweathermap.org/api_keys
-weather_api_key = "86cb9788ec874ce3961bfd73a5fc6302"
-
-# Google API Key
-g_key = "AIzaSyDaJdE8aypOBq_mNQ9SQ6f_ZW6GIhWvZCQ"
